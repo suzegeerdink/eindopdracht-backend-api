e6745f9a6c921338cb52e4976634e063fc064c65
suzegeerdink <geerdinksuze@gmail.com>
2026-05-07 15:27:52 +0200

    chore: add project files

---
+++ src/main/java/nl/novi/eindopdrachtbackendapi/EindopdrachtBackendApiApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.eindopdrachtbackendapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EindopdrachtBackendApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EindopdrachtBackendApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/novi/eindopdrachtbackendapi/EindopdrachtBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.eindopdrachtbackendapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EindopdrachtBackendApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
