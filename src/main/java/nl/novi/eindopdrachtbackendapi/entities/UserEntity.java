d6a5264cff370770ba131fcf84451b838e032c99
suzegeerdink <geerdinksuze@gmail.com>
2026-05-08 14:55:22 +0200

    feat: add UserEntity and UserRepository

---
+++ src/main/java/nl/novi/eindopdrachtbackendapi/entities/UserEntity.java
@@ -0,0 +1,63 @@
+package nl.novi.eindopdrachtbackendapi.entities;
+
+import jakarta.persistence.*;
+import nl.novi.eindopdrachtbackendapi.enums.Role;
+
+@Entity
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    public UserEntity() {}
+
+    public UserEntity(String email, String password, Role role) {
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
---
+++ src/main/java/nl/novi/eindopdrachtbackendapi/enums/Role.java
@@ -0,0 +1,5 @@
+package nl.novi.eindopdrachtbackendapi.enums;
+
+public enum Role {
+    USER, ADMIN, CONTENTMANAGER
+}
