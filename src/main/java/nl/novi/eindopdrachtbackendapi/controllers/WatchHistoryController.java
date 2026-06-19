79cb8e528dccb79a5e87a2f38d54183f2a778d8a
suzegeerdink <geerdinksuze@gmail.com>
2026-06-19 14:42:34 +0200

    Add WatchHistoryController with CRUD endpoints

---
+++ src/main/java/nl/novi/eindopdrachtbackendapi/controllers/WatchHistoryController.java
@@ -0,0 +1,51 @@
+package nl.novi.eindopdrachtbackendapi.controllers;
+
+import nl.novi.eindopdrachtbackendapi.dtos.watchhisory.WatchHistoryRequestDTO;
+import nl.novi.eindopdrachtbackendapi.dtos.watchhisory.WatchHistoryResponseDTO;
+import nl.novi.eindopdrachtbackendapi.services.WatchHistoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/watch-history")
+public class WatchHistoryController {
+
+    private final WatchHistoryService watchHistoryService;
+
+    public WatchHistoryController(WatchHistoryService watchHistoryService) {
+        this.watchHistoryService = watchHistoryService;
+    }
+
+    @PostMapping
+    public ResponseEntity<WatchHistoryResponseDTO> createWatchHistory(@RequestBody WatchHistoryRequestDTO watchHistoryRequestDTO) {
+        WatchHistoryResponseDTO createdWatchHistory = watchHistoryService.createWatchHistory(watchHistoryRequestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdWatchHistory);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<WatchHistoryResponseDTO>> getAllWatchHistory() {
+        List<WatchHistoryResponseDTO> getAllWatchHistories = watchHistoryService.getAllWatchHistories();
+        return ResponseEntity.ok().body(getAllWatchHistories);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<WatchHistoryResponseDTO> getWatchHistoryById(@PathVariable Long id) {
+        WatchHistoryResponseDTO getWatchHistoryById = watchHistoryService.getWatchHistoryById(id);
+        return ResponseEntity.ok().body(getWatchHistoryById);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<WatchHistoryResponseDTO> updateWatchHistory(@PathVariable Long id, @RequestBody WatchHistoryRequestDTO watchHistoryRequestDTO) {
+        WatchHistoryResponseDTO updatedWatchHistory = watchHistoryService.updateWatchHistory(id, watchHistoryRequestDTO);
+        return ResponseEntity.ok().body(updatedWatchHistory);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteWatchHistory(@PathVariable Long id) {
+        watchHistoryService.deleteWatchHistory(id);
+        return ResponseEntity.noContent().build();
+    }
+}
